f44b6d177162d22ba141932a6beff284ed127297
Alpha-Gruis <sten.vallimae@gmail.com>
2021-05-22 23:21:25 +0300

    faq section

--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
@@ -56,7 +56,6 @@ p {
         line-height: 150%;
     }
 
-
-`
+`;
 
 export default GlobalStyle;
