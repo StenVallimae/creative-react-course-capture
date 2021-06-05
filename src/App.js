a36cf552923c5ec9158173b7bf4a30378090036f
Alpha-Gruis <sten.vallimae@gmail.com>
2021-06-05 14:41:42 +0300

    8. React router dom, switch and route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import AboutUs from "./Pages/AboutUs";
 import GlobalStyle from "./Components/GlobalStyle";
 import Nav from "./Components/Nav";
+import ContactUs from "./Pages/ContactUs";
+import OurWork from "./Pages/OurWork";
+//router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
